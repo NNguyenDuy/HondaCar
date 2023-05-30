1c33075534b3261b5ff0f96536b2b77f580556a6
NguyenDuy <duydadaga1@gmail.com>
2023-05-30 12:46:02 +0700

    doneProjectV1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Nguyễn Duy
 // Menu Navbar Respontive
 let menubar = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.nav-bar');
@@ -7,7 +8,7 @@ function toggleBar() {
   navbar.classList.toggle('active');
 }
 
-// Header scroll
+// Header scroll and move on header
 window.onscroll = () => {
   let header = document.querySelector(".header");
   let activeMovePage = document.querySelector(".active-move-page");
@@ -27,10 +28,10 @@ window.onscroll = () => {
 // Header user
 let userNav = document.querySelector(".nav-bar-account");
 document.querySelectorAll(".user-active").forEach(function (element) {
-  element.addEventListener("click", hihi);
+  element.addEventListener("click", user);
 });
 
-function hihi() {
+function user() {
   userNav.style.opacity = "1";
   userNav.style.display = "block";
 }
@@ -40,7 +41,7 @@ document.getElementById("button-user").onclick = () => {
   userNav.style.display = "none";
 };
 
-// Choose car
+// Choose car vehicles
 let sizeC = 2, localC = 0;
 const images = document.querySelectorAll(".st3-car img");
 document.querySelector("#choose-carR").onclick = () => {
@@ -74,48 +75,19 @@ function changeSAO(value, operator) {
   document.getElementById(`imgcar${eval(value + operator + 1)}`).style.width = "36rem";
 }
 
-
-// Sơn
-const colorOptions = document.querySelectorAll('.color-option');
-const carPreview = document.querySelector('.car-preview');
-
-colorOptions.forEach(colorOption => {
-  colorOption.addEventListener('click', () => {
-    const selectedColor = colorOption.getAttribute('data-color');
-    carPreview.style.backgroundColor = selectedColor;
-  });
-});
-
-//Son
-const motors = [];
-
-// Ảnh xe 1
-const motor0 = [];
-motor0.push({ Info: "The owner of a white car is usually an extremely orderly person, who values ​​order and honesty both in personal and professional life.", imgURL: "./color_xe/1.png" });
-
-//Ảnh xe 2
-const motor1 = [];
-motor0.push({ Info: "Gray is often associated with formality, wisdom and understanding; Therefore, users of gray cars are often said to be pragmatic, conservative, elegant and self-contained.", imgURL: "./color_xe/2.png" });
-
-// Ảnh xe 3
-const motor2 = [];
-motor0.push({ Info: "Driving a black car often sends out a message in the perception that the owner is a powerful and confident person who can control their own world well.", imgURL: "./color_xe/3.png" });
-
-//Ảnh xe 4
-const motor3 = [];
-motor0.push({ Info: "Strong personality, ambitious, full of passion and inspiration. This is the type of person who likes to do things quickly, is enthusiastic at work and often focuses on personal interests.", imgURL: "./color_xe/4.png" });
-
-motors.push(motor0);
-motors.push(motor1);
-motors.push(motor2);
-motors.push(motor3);
-
-function pickColor(i, j) {
-  document.getElementById("Color").src = motors[i][j].imgURL;
-  document.getElementById("Info").innerHTML = motors[i][j].Info;
+// Trường Sơn
+// Blog 
+function imgBlog(i) {
+  document.getElementById("blog").style.backgroundImage = `url('./imagesBlog/xe${i}.jpeg')`;
 }
 
-// Trường
+// Car
+function pickColor(i) {
+  document.getElementById("Color").src = `./color_xe/${i}.png`;
+}
+
+// Xuân Trường
+// Shop car 
 document.getElementById('searchInput').addEventListener('input', function () {
   var searchValue = this.value.toLowerCase();
   var productItems = document.getElementsByClassName('Card');
